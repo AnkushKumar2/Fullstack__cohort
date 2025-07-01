36f927ca9e8c24bf1626a2386afec4b8dc72f146
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-07-01 23:51:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+import express, { response } from "express";
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello cohort!')
+})
+
+app.get('/Ankush',(req, res) =>{
+    res.send('hello ankush!')
+})
+
+app.get('/hey',(request, response) => {
+    res.send('hiiiiii ankush')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
