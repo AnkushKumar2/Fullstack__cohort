14adbe65a52e1d98050ca081a06e98bc90efe1c1
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-07-03 16:18:24 +0530

    connecting database

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import db from "./utilities/db.js";
 
 dotenv.config()
 
 
-const app = express()
+const app = express();
 
-app.use(cors({
-  origin:"http://localhost:3000",
+app.use(
+  cors({
+  origin: process.env.BASE_URL,
   credentials:true,
   methods:['GET','DELETE','OPTIONS'],
   allowedheaders:['Content-type','Authorization']
@@ -30,7 +32,9 @@ app.get('/hey1',(request, response) => {
     res.send('hiiiiii ankush')
 })
 
-console.log();
+// console.log();
+//connect to db
+db();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- utilities/db.js
+++ utilities/db.js
@@ -1,3 +1,19 @@
 import mongoose from "mongoose";
 
-mongoose.connect()
+import dotenv from "dotenv"
+dotenv.config()
+
+const db = ()=>{
+    mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log(()=>{
+        console.log("connected to mongodb")
+    })
+})
+.catch((err)=>{
+    console.log("error connecting to mongobd")
+})
+}
+
+export default db;
