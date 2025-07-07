c061e6cd0ee13bd3d04e98df9f2e5702b11e7ada
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-07-07 23:41:14 +0530

    authentication

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,5 +1,36 @@
 const registerUser = async(req,res)=>{
-    res.send("registered")
+//get data
+
+//validate
+
+
+//check if user already exists
+
+
+//creat a user in database
+
+
+
+//create a verification token
+
+
+//save token in database
+
+
+//send token as email to user
+
+
+//send success status to user
+
+const{name,email,password} =  req.body
+
+if(!name|| !email|| !password){
+    return res.status(400).json({
+        message:"All fields are required"
+    });
+}
+
+
 }
 
 export{registerUser}
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
   res.send('Hello cohort!')
